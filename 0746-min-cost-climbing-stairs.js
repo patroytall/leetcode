2cfebcb1c1dc1e805881cd515f8628038dcca291
Patrick Roy <patrick.roy@usmobile.com>
2022-05-18 04:05:32 -0400

    Min cost climbing stairs

---
+++ 0746-min-cost-climbing-stairs.js
@@ -0,0 +1,64 @@
+/* You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+
+You can either start from the step with index 0, or the step with index 1.
+
+Return the minimum cost to reach the top of the floor.
+
+Example 1:
+
+Input: cost = [10,15,20]
+Output: 15
+Explanation: You will start at index 1.
+- Pay 15 and climb two steps to reach the top.
+The total cost is 15.
+
+Example 2:
+
+Input: cost = [1,100,1,1,1,100,1,1,100,1]
+Output: 6
+Explanation: You will start at index 0.
+- Pay 1 and climb two steps to reach index 2.
+- Pay 1 and climb two steps to reach index 4.
+- Pay 1 and climb two steps to reach index 6.
+- Pay 1 and climb one step to reach index 7.
+- Pay 1 and climb two steps to reach index 9.
+- Pay 1 and climb one step to reach the top.
+The total cost is 6.
+ 
+
+Constraints:
+
+2 <= cost.length <= 1000
+0 <= cost[i] <= 999 */
+
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+function minCostClimbingStairs(cost) {
+    let first = cost[0];
+    let second = cost[1]
+    for (let i = 2; i < cost.length; ++i) {
+        const currentSecond = second;
+        if (first + cost[i] < second + cost[i]) {
+            second = first + cost[i];
+        } else {
+            second += cost[i];
+        }
+        first = currentSecond;
+    }
+    return first < second ? first : second;
+}
+
+[
+    { input: [1,2], output: 1 },
+    { input: [2,1], output: 1 },
+    { input: [10,15,20], output: 15 },
+    { input: [1,100,1,1,1,100,1,1,100,1], output: 6 },
+].forEach(e => {
+    const output = minCostClimbingStairs(e.input);
+    if (output !== e.output) {
+        throw Error(`input: ${e.input} - actual output: ${output} - expected output: ${e.output}`);
+    }
+})
+console.log("success")
