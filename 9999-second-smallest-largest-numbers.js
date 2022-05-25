e42052d7aa8ec431d673e756cca26779c9e49fe0
Patrick Roy <patrick.roy@usmobile.com>
2022-05-25 12:23:23 -0400

    Cleaner code

--- 9999-second-smallest-largest-numbers.js
+++ 9999-second-smallest-largest-numbers.js
@@ -17,37 +17,39 @@ Constraints:
 1 <= items.length <= 2^10 */
 
 function secondSmallestAndLargest(nums) {
-  if (nums.length === 1) {
-      return [nums[0], nums[0]];
-  }
-  let smallest1 = nums[0] <= nums[1] ? nums[0] : nums[1];
-  let smallest2 = nums[0] <= nums[1] ? nums[1] : nums[0];
-  let largest1 = nums[0] <= nums[1] ? nums[1] : nums[0];
-  let largest2 = nums[0] <= nums[1] ? nums[0] : nums[1];
-  for (var i = 2; i < nums.length; i++) {
-      const n = nums[i];
-      if (n < smallest1) {
-          smallest2 = smallest1
-          smallest1 = n;
-      } else if (n < smallest2 && n !== smallest1) {
-          smallest2 = n;
-      }
-      if (n > largest1) {
-          largest2 = largest1;
-          largest1 = n;
-      } else if (n > largest2 && n !== largest1) {
-          largest2 = n;
-      }
-  }
-  return [smallest2, largest2];
+    const num0 = nums[0];
+    if (nums.length === 1) {
+        return [num0, num0];
+    }
+    const num1 = nums[1];
+    let smallest1 = num0 <= num1 ? num0 : num1;
+    let smallest2 = num0 <= num1 ? num1 : num0;
+    let largest1 = num0 <= num1 ? num1 : num0;
+    let largest2 = num0 <= num1 ? num0 : num1;
+    for (let i = 2; i < nums.length; i++) {
+        const n = nums[i];
+        if (n < smallest1) {
+            smallest2 = smallest1
+            smallest1 = n;
+        } else if (n < smallest2 && n !== smallest1) {
+            smallest2 = n;
+        }
+        if (n > largest1) {
+            largest2 = largest1;
+            largest1 = n;
+        } else if (n > largest2 && n !== largest1) {
+            largest2 = n;
+        }
+    }
+    return [smallest2, largest2];
 }
 
 [
     { nums: [1], output: [1, 1] },
     { nums: [-1, 0, 1], output: [0, 0] },
     { nums: [-1, 1], output: [1, -1] },
-    { nums: [1,-1,5,-6], output: [-1, 1] },
-    { nums: [1,-1,-6,5,7,-6,7], output: [-1, 5] },
+    { nums: [1, -1, 5, -6], output: [-1, 1] },
+    { nums: [1, -1, -6, 5, 7, -6, 7], output: [-1, 5] },
 ].forEach(e => {
     const output = secondSmallestAndLargest(e.nums);
     if (output[0] !== e.output[0] || output[1] !== e.output[1]) {
